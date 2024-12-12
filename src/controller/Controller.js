3f783e3a7626dff50fe406fa7afb9a70ff6d02c6
kimyou1102 <yoosk5485@naver.com>
2024-12-12 17:11:32 +0900

    feat: 입력받은 길이만큼 다리 생성

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -1,9 +1,12 @@
 import InputView from '../view/InputView.js';
 import OutputView from '../view/OutputView.js';
+import BridgeMaker from '../BridgeMaker.js';
+import BridgeRandomNumberGenerator from '../BridgeRandomNumberGenerator.js';
 
 export default class Controller {
   async start() {
     OutputView.printGreeting();
     const length = await InputView.readBridgeSize();
+    const bridge = BridgeMaker.makeBridge(length, BridgeRandomNumberGenerator.generate);
   }
 }
