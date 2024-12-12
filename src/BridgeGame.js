41357f0c8f175ee0dac92c489caeb855d96f8407
kimyou1102 <yoosk5485@naver.com>
2024-12-12 18:45:35 +0900

    refactor: 속성명 직관적으로 변경

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -4,7 +4,7 @@
 export default class BridgeGame {
   constructor(bridge) {
     this.bridge = bridge;
-    this.try = 0;
+    this.tryCount = 0;
     this.result = [];
   }
 
@@ -27,7 +27,7 @@ export default class BridgeGame {
    * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   retry() {
-    this.try += 1;
+    this.tryCount += 1;
     this.result = [];
   }
 
@@ -36,6 +36,6 @@ export default class BridgeGame {
   }
 
   getGameResult() {
-    return { try: this.try, result: this.result };
+    return { tryCount: this.tryCount, result: this.result };
   }
 }
