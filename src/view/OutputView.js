4bb4b25a8e9e34ffbec85e310d84a5d18cf3af80
kimyou1102 <yoosk5485@naver.com>
2024-12-12 16:54:55 +0900

    feat: OutputView 클래스 생성
    - 인사말 출력 메서드 생성
    - 에러 메시지 출력 메서드 생성

---
+++ src/view/OutputView.js
@@ -0,0 +1,16 @@
+import { Console } from '@woowacourse/mission-utils';
+
+export default class OutputView {
+  static printGreeting() {
+    Console.print('다리 건너기 게임을 시작합니다.');
+  }
+  // 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력
+  static printMap() {}
+
+  // 게임의 최종 결과를 정해진 형식에 맞춰 출력
+  static printResult() {}
+
+  static printError(message) {
+    Console.print(message);
+  }
+}
