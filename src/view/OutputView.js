032b8a0b0b392cea1a505bb0ea21ea181f99383e
kimyou1102 <yoosk5485@naver.com>
2024-12-12 16:56:23 +0900

    feat: 안내 문구 출력 구현

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -2,7 +2,7 @@ import { Console } from '@woowacourse/mission-utils';
 
 export default class OutputView {
   static printGreeting() {
-    Console.print('다리 건너기 게임을 시작합니다.');
+    Console.print('다리 건너기 게임을 시작합니다.\n');
   }
   // 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력
   static printMap() {}
